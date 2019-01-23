ee241b6f329714ca9958a718f431cfb31ff9eb8d
kisdani <kisdani0720@gmail.com>
2019-01-23 20:29:37 +0100

    1. Lesson

---
+++ myApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// futtatás dotnet run
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
